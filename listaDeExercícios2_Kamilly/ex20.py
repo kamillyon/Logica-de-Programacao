5e95d1a41e2bf9dfb09d56307197b60650f80241
Kamilly <kakalrrosa@gmail.com>
2026-05-27 18:12:37 -0300

    Create ex20.py

---
+++ listaDeExercícios2_Kamilly/ex20.py
@@ -0,0 +1,4 @@
+print("=== Inversor de Chamadas ===")
+alunos = ["Mel", "Lua", "Milly", "Joe", "Meggy"] #O primeiro nome corresponde ao primeiro que chegou e o último nome corresponde ao último que chegou
+alunos_inverso = alunos[::-1]
+print(f"Nomes do primeiro a chegar até o último: {alunos}", f"Nomes do último a chegar até o primeiro: {alunos_inverso}")
